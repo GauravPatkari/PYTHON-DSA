8a67735a1b3ed66ef6246d633139d9017b5cf04e
Gaurav_K_Patkari <88868677+GauravPatkari@users.noreply.github.com>
2024-11-08 13:21:16 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,25 @@
+''' Dictionary :- In python is a dta sturcture, use dto store values in [ key: value] format. This make it different from list, tuples and arrays.
+                  In dictionary each key has an assoiated value.
+                  A dictionary is crated by placing a sequence o elemtns within curyly {} braces separated by 'comma'.
+                  Which hold the pair element being its Key:Value.
+                  Where "Value" in dictionary can be of any data type and can be duplicated, where as key can't be reaeated and must be immutable.
+                  
+'''
+
+Dict = {1:'geeks', 2:'for', 3:'geeks'}
+print("\nDictionary with the use of IntegerKey:")
+print(Dict)
+
+
+
+dict = {'Name': 'Gaurav', 1:[1,2,3,4]}
+print("\nDictionary iwht the use of Mied Keys:")
+print(dict)
+
+
+''' Creations of Emtpy Directory'''
+dict={}
+print("Empty Dictionary:")
+print(dict)
+
 
