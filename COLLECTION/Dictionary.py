4e7e59aedb1b807936ce62ade549ca4741dd5487
Gaurav-Patkari <gaurav.patkari@krawlnet.com>
2025-10-31 12:58:36 +0530

    Created a new dir and moved into it

---
+++ COLLECTION/Dictionary.py
@@ -0,0 +1,25 @@
+''' Dictionary :- In python is a data structure, use data to store values in [ key: value] format. This make it different from list, tuples and arrays.
+                  In dictionary each key has an associated value.
+                  A dictionary is created by placing a sequence o elements within curyly {} braces separated by 'comma'.
+                  Which hold the pair element being its Key:Value.
+                  Where "Value" in dictionary can be of any data type and can be duplicated, where as key can't be recreated and must be immutable.
+                  
+'''
+
+Dict = {1:'geeks', 2:'for', 3:'geeks'}
+print("\nDictionary with the use of IntegerKey:")
+print(Dict)
+
+
+
+dict = {'Name': 'Gaurav', 1:[1,2,3,4]}
+print("\nDictionary with the use of Mixed Keys:")
+print(dict)
+
+
+''' Creations of Emtpy Directory'''
+dict={}
+print("Empty Dictionary:")
+print(dict)
+
+
